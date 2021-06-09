20040c256062efd71b7504d127700f76c7d91e04
Kkmiloo <camilo.reyes_Granada@uao.edu.co>
2021-06-08 21:57:42 -0500

    archivo sin trastes

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ function iniciarSesion(){
     let password = document.getElementById("password");
     const mensajerError = document.getElementById("mensaje_error");
 
-    if(correo.value == "usuario" && password.value == "usuario"){
+    if(correo.value == "usuario@gmail.com" && password.value == "usuario123"){
+        console.log("correcto");
+        window.open("./seleccion.html", "_self");
+    }else if(correo.value == "emcali@gmail.com" && password.value == "emcali123"){
         console.log("correcto");
         window.open("./seleccion.html", "_self");
     }else{
